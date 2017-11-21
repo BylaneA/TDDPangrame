66ea9928357a8d0d742a5148fc735199ca6e2629
BylaneA <billieabdiw@gmail.com>
2017-11-21 20:19:34 +0100

    function with 2 failures on 4

--- pangram.js
+++ pangram.js
@@ -10,11 +10,13 @@ function Pangram(textToTest) {
     this.textToTest = textToTest;
 
     this.isPangram = function () {
+    	this.textToTest= this.textToTest.toLowerCase();
+    	for (var i = 0; i < this.textToTest.length; i++) {
+			if (this.textToTest.includes("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")){
+				return true;
+			}
 
-
-        /*
-            Put your code here, you can access the code in variable : this.textToTest
-        */
-
-  }
-}
+			return false;
+		}
+ 	}
+};
